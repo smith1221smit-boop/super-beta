1a1f4456db74c27654d032ad020d0cf1a3567758
unknown <smitthapa808@gmail.com>
2026-04-20 22:18:25 +0545

    ea

--- src/login/api.tsx
+++ src/login/api.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://ramus-back-ehz9.onrender.com/api",
+  baseURL: "https://super-back-osc7.onrender.com/api",
   withCredentials: true,  // MUST BE HERE ONLY
   headers: {
     'Content-Type': 'application/json',

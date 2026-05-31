89caffb08f0b679703d56f902dd4d198171959f1
unknown <smitthapa808@gmail.com>
2026-05-31 14:26:36 +0545

    new rmaus

--- src/dashboard/socketManager.tsx
+++ src/dashboard/socketManager.tsx
@@ -22,7 +22,7 @@ class SocketManager {
 
     if (!this.socket || this.socket.disconnected) {
       console.log('SocketManager: Creating new socket connection');
-      this.socket = io("https://super-back-507l.onrender.com/", {
+      this.socket = io("https://ramus-back-40fi.onrender.com", {
         transports: ["websocket"],
         withCredentials: true,
         reconnection: true,

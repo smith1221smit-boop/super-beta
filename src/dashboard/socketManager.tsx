e97d691f4e5fbc5c1c7c97404bb282bb757618ae
unknown <smitthapa808@gmail.com>
2026-06-22 19:54:36 +0545

    server

--- src/dashboard/socketManager.tsx
+++ src/dashboard/socketManager.tsx
@@ -22,7 +22,7 @@ class SocketManager {
 
     if (!this.socket || this.socket.disconnected) {
       console.log('SocketManager: Creating new socket connection');
-      this.socket = io("https://ramus-back-40fi.onrender.com", {
+      this.socket = io("https://super-back-4xur.onrender.com", {
         transports: ["websocket"],
         withCredentials: true,
         reconnection: true,
--- src/login/api.tsx
+++ src/login/api.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://ramus-back-40fi.onrender.com/api",
+  baseURL: "https://super-back-4xur.onrender.com/api",
   withCredentials: true,  // MUST BE HERE ONLY
   headers: {
     'Content-Type': 'application/json',

05dbb76aa6ba40002d874d5b1b76311ec20c759d
unknown <smitthapa808@gmail.com>
2026-07-06 20:07:54 +0545

    new

--- src/dashboard/socketManager.tsx
+++ src/dashboard/socketManager.tsx
@@ -22,7 +22,7 @@ class SocketManager {
 
     if (!this.socket || this.socket.disconnected) {
       console.log('SocketManager: Creating new socket connection');
-      this.socket = io("https://ramus-back-jz8i.onrender.com", {
+      this.socket = io("https://ramus-back-kdxu.onrender.com", {
         transports: ["websocket"],
         withCredentials: true,
         reconnection: true,

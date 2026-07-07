fd942129d73bcefdaa0924e51eacf1d07e90db1b
unknown <smitthapa808@gmail.com>
2026-07-07 23:37:26 +0545

    new

--- src/dashboard/socketManager.tsx
+++ src/dashboard/socketManager.tsx
@@ -22,7 +22,7 @@ class SocketManager {
 
     if (!this.socket || this.socket.disconnected) {
       console.log('SocketManager: Creating new socket connection');
-      this.socket = io("https://super-back-fq5a.onrender.com", {
+      this.socket = io("https://super-back-507l.onrender.com", {
         transports: ["websocket"],
         withCredentials: true,
         reconnection: true,

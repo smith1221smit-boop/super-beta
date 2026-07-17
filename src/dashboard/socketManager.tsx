a58464ea5a8719fae5bcfac5a2a0bdd443041a61
unknown <smitthapa808@gmail.com>
2026-07-17 23:10:40 +0545

    eaas

--- src/dashboard/socketManager.tsx
+++ src/dashboard/socketManager.tsx
@@ -22,7 +22,7 @@ class SocketManager {
 
     if (!this.socket || this.socket.disconnected) {
       console.log('SocketManager: Creating new socket connection');
-      this.socket = io("https://new-back-6ko0.onrender.com", {
+      this.socket = io("https://socket-back-g5wi.onrender.com", {
         transports: ["websocket"],
         withCredentials: true,
         reconnection: true,

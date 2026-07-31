b7ad8c478083937aabc74a9b48fabd5d8cc49edd
unknown <smitthapa808@gmail.com>
2026-07-31 22:30:53 +0545

    new

--- src/login/api.tsx
+++ src/login/api.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://beta-server-back-4mu5.onrender.com/api",
+  baseURL: "https://ramus-back-1.onrender.com/api",
   withCredentials: true,  // MUST BE HERE ONLY
   headers: {
     'Content-Type': 'application/json',

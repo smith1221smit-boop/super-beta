a69a94039997f5f2ce812c15805e62516ba58cf8
unknown <smitthapa808@gmail.com>
2026-08-20 13:31:17 +0545

    new

--- src/login/api.tsx
+++ src/login/api.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://super-beta-back-1-lmf2.onrender.com/api",
+  baseURL: "https://super-beta-back-1-cue8.onrender.comapi",
   withCredentials: true,  // MUST BE HERE ONLY
   headers: {
     'Content-Type': 'application/json',
